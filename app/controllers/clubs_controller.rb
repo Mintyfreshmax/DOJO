ae27969eb8141a32e8dca75166cc2c8d92d02473
madeleine de kerchove <madeleine@de-kerchove.be>
2024-11-29 12:18:48 +0100

    working progress

--- app/controllers/clubs_controller.rb
+++ app/controllers/clubs_controller.rb
@@ -1,4 +1,5 @@
 class ClubsController < ApplicationController
+  skip_before_action :authenticate_user!, only: :show
   def show
     @club = Club.find(params[:id])
     @club.details = "This is a sample description for the club. It contains detailed information about the club's activities, events, and other relevant information that members might find useful. The description is long enough to test the 'Learn More' functionality, which will reveal the rest of the text when clicked."
