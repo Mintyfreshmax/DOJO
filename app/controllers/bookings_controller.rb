0fdd1ce5f5fc4cd70cc91d030b694319be5a7245
Sandor Lazar <sandorlazar2@gmail.com>
2024-11-29 15:16:59 +0100

    Bookings done

---
+++ app/controllers/bookings_controller.rb
@@ -0,0 +1,30 @@
+class BookingsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_activity, only: :create
+
+  def create
+    @booking = Booking.new(booking_params)
+    @booking.user = current_user
+    @booking.activity = @activity
+    if @booking.save
+      flash[:success] = "Congrats 🎉 You successfully booked the activity #{@activity.title} at the dojo #{@activity.club.name}.
+      Meeting will happen at the address #{@activity.address} on #{@activity.event_time.strftime("%a %b %e at %l:%M %p")}"
+      puts flash[:success] # Debugging output
+      redirect_to activity_path(@activity)
+    else
+      flash[:alert] = "Your booking was unsuccessful, please try again"
+      puts flash[:alert] # Debugging output
+      render "activities/show", status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def set_activity
+    @activity = Activity.find(params[:activity_id])
+  end
+
+  def booking_params
+    params.fetch(:booking, {}).permit(:start_date, :end_date)
+  end
+end
