380592f3263d41378ce9660b8c1d17e50dec870b
Sandor Lazar <sandorlazar2@gmail.com>
2024-12-05 16:43:13 +0100

    Improved feed and a few css elements

--- app/javascript/controllers/activities_feed_toggle_controller.js
+++ app/javascript/controllers/activities_feed_toggle_controller.js
@@ -2,25 +2,25 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="activities-feed-toggle"
 export default class extends Controller {
-  static targets = ["activities", "feed"];
+  static targets = ["activities", "feed", "form", "input"];
 
   connect() {
     console.log("Activities and Feed toggle controller connected!");
   }
 
   showActivities(event) {
-    this.toggleActiveButton(event.target);
+    this._toggleActiveButton(event.target);
     this.activitiesTarget.classList.remove("d-none");
     this.feedTarget.classList.add("d-none");
   }
 
   showFeed(event) {
-    this.toggleActiveButton(event.target);
+    this._toggleActiveButton(event.target);
     this.feedTarget.classList.remove("d-none");
     this.activitiesTarget.classList.add("d-none");
   }
 
-  toggleActiveButton(target) {
+  _toggleActiveButton(target) {
     const buttons = target.closest("#club-activities-feed-buttons").querySelectorAll(".activities-title-index");
     buttons.forEach((button) => button.classList.remove("active"));
     target.classList.add("active");
